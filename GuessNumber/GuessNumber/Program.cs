3f3f3d57a4a6c848a5c5b179ce8b8f598e886831
TimmyLuong <timmyluong11@gmail.com>
2021-02-25 00:20:49 -0600

    Finished code clean up. Added comments to each section. Guessing Number is done.

--- GuessNumber/GuessNumber/Program.cs
+++ GuessNumber/GuessNumber/Program.cs
@@ -6,13 +6,17 @@ namespace GuessNumber
     {
         static void Main(string[] args)
         {
+            //Timmy Luong - 113401608
+            //Declaring Variables
             Random rand;
             rand = new Random();
             int min, max, ranNum, guess=0, count=0;
             string answer, minAns, maxAns;
 
+            //Welcoming the user to a guessing game
             Console.WriteLine("Welcome to the number guessing game!");
             
+            //Asking the user to enter in a valid minimum number, if not it will loop until valid
             Console.WriteLine("Please enter in a minimum number");
             minAns = Console.ReadLine();
             while (int.TryParse(minAns, out min) == false)
@@ -21,7 +25,8 @@ namespace GuessNumber
                 minAns = Console.ReadLine();
 
             }
-            
+
+            //Asking the user to enter in a valid maximum number, if not it will loop until valid
             Console.WriteLine("Please enter in a maximum number");
             maxAns = Console.ReadLine();
             while (int.TryParse(maxAns, out max)==false)
@@ -30,11 +35,11 @@ namespace GuessNumber
                 maxAns = Console.ReadLine();
             }
             
+            //Generating random number from the min and max value from the user with the +1 to include the max number
             ranNum = rand.Next(min, max+1);
-            Console.WriteLine($"This is the random Num {ranNum}");
             
+            //Asking the user to start guessing and if the input is not valid it will ask the user to try agian
             Console.WriteLine("Please enter in your guess");
-
             do
             {
                 answer = Console.ReadLine();
@@ -48,8 +53,10 @@ namespace GuessNumber
 
             } while (guess != ranNum);
 
+            //Outputting to the user they have guess correctly, the correct number, and how many times it took
             Console.WriteLine("You guess correctly!");
-            Console.WriteLine($"It took you {count} times to guess correctly!");
+            Console.WriteLine($"The correct number was: {ranNum}.");
+            Console.WriteLine($"It took you {count} tries to guess correctly!");
         }
     }
 }
